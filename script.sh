b16bc1778dd9159aea7b7f8f0f86ef04a7633058
xDev <papesock@yahoo.fr>
2024-01-25 16:23:00 +0100

    add script

--- script.sh
+++ script.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+while true; do
+        status=$(kubectl get pod -l app=gowebapi -n developpement -o=jsonpath='{.items[0].status.phase}')
+        if [ "$status" == "Running" ]; then
+            echo "Le pod gowebapi est en cours d'exécution."
+            break
+        else
+            echo "En attente que le pod MongoDB soit en cours d'exécution..."
+            sleep 2000
+        fi
+    done
+    
 a=$(minikube service gowebapi --url -n developpement)
 curl "$a/whoami"
